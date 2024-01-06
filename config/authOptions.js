df25e1965ddbdf50e7a1cde6b4affcccd101e850
Inderjeet Singh <inder12801@gmail.com>
2024-01-07 00:24:23 +0530

    modified:   prisma/schema.prisma

--- config/authOptions.js
+++ config/authOptions.js
@@ -1,11 +1,16 @@
-import prisma from "@/prisma";
+import prisma from "../prisma/index";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { getToken } from "next-auth/jwt";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
-  adapter: PrismaAdapter(prisma),
-  session: { strategy: "jwt" },
+  // adapter: PrismaAdapter(prisma),
+  // session: { strategy: "jwt" },
+  // jwt: {
+  //   secret: process.env.NEXTAUTH_SECRET,
+  //   encryption: true,
+  //   signingKey: process.env.NEXTAUTH_SECRET,
+  // },
   // Configure one or more authentication providers
   providers: [
     // GithubProvider({
@@ -16,58 +21,60 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      profile(profile) {
-        // console.log(profile);
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          profilePic: profile.picture,
-        };
-      },
+      // profile(profile) {
+      //   // console.log(profile);
+      //   return {
+      //     id: profile.sub,
+      //     name: profile.name,
+      //     email: profile.email,
+      //     profilePic: profile.picture,
+      //   };
+      // },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    session: async (session, user) => {
-      // console.log("session : ", session);
-      // console.log({ user });
-      return session;
-    },
-    jwt: async ({ token, user }) => {
-      console.log("token : ", token);
-      return { ...token, ...user };
-    },
-    signIn: async ({ session, user, account, profile, email, credentials }) => {
-      // console.log(user, profile, email, credentials);
-      // console.log("session : ", session);
-      // console.log("user : ", user);
-      // console.log("account : ", account);
-      // console.log("profile : ", profile);
-      // console.log("email : ", email);
-      // console.log("credentials : ", credentials);
-      // check if user already exists in db
-      const existUser = await prisma.user.findUnique({
-        where: {
-          email: profile.email,
-        },
-      });
-      console.log(existUser);
-      if (!existUser) {
-        // // if not, create user
-        const newUser = await prisma.user.create({
-          data: {
-            email: profile.email,
-            name: profile.name,
-            profilePic: profile.profilePic,
-            id: profile.id,
-          },
-        });
-        console.log(newUser);
-      }
+  // secret: process.env.NEXTAUTH_SECRET,
+  // callbacks: {
+  //   session: async (session, user) => {
+  //     // console.log("session : ", session);
+  //     // console.log({ user });
+  //     return session;
+  //   },
+  // jwt: async ({ token, user }) => {
+  //   console.log("token : ", token);
+  //   // const token = await getToken();
+  //   return { ...token };
+  // },
+  // signIn: async ({ session, user, account, profile, email, credentials }) => {
+  // console.log(user, profile, email, credentials);
+  // console.log("session : ", session);
+  // console.log("user : ", user);
+  // console.log("account : ", account);
+  // console.log("profile : ", profile);
+  // console.log("email : ", email);
+  // console.log("credentials : ", credentials);
+  // check if user already exists in db
 
-      // return true to accept the login
-      return true;
-    },
-  },
+  //     const existUser = await prisma.user.findUnique({
+  //       where: {
+  //         email: profile.email,
+  //       },
+  //     });
+  //     console.log(existUser);
+  //     if (!existUser) {
+  //       // // if not, create user
+  //       const newUser = await prisma.user.create({
+  //         data: {
+  //           email: profile.email,
+  //           name: profile.name,
+  //           profilePic: profile.profilePic,
+  //           id: profile.id,
+  //         },
+  //       });
+  //       console.log(newUser);
+  //     }
+
+  //     // return true to accept the login
+  //     return true;
+  //   },
+  // },
 };
